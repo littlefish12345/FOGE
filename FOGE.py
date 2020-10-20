90358583a7e39ccab86d514ed8a10956b31d2dd8
little_fish12345 <18819303050@163.com>
2020-10-20 20:49:40 +0800

    临时保存

--- FOGE.py
+++ FOGE.py
@@ -17,10 +17,57 @@ def __fogeProcessKeyInput__(window): #处理键盘输入
 def __fogeDrawObj__():
     global draw_obj
     for obj in draw_obj:
+        glBindVertexArray(obj.VAO)
         if obj.type == 'triangle':
-            glBindVertexArray(obj.VAO)
             glDrawArrays(GL_TRIANGLES,0,3)
-            glBindVertexArray(0)
+        else:
+            glDrawElements(GL_TRIANGLES,obj.indices_num,GL_UNSIGNED_INT,None)
+        glBindVertexArray(0)
+
+class __fogeShader__:
+    def __init__(self,vertex_path,fragment_path):
+        vertexShaderSourceFile = open(vertex_path,'r')
+        vertexShaderSource = vertexShaderSourceFile.read()
+        vertexShaderSourceFile.close()
+        vertexShader = glCreateShader(GL_VERTEX_SHADER)
+        glShaderSource(vertexShader,vertexShaderSource)
+        glCompileShader(vertexShader)
+        vertexCompileSuccess = glGetShaderiv(vertexShader,GL_COMPILE_STATUS)
+        if not vertexCompileSuccess:
+            infoLog = glGetShaderInfoLog(vertexShader)
+            logging.critical(infoLog)
+            raise 'Unable to compile vertex shader'
+
+        fragmentShaderSourceFile = open(fragment_path,'r')
+        fragmentShaderSource = fragmentShaderSourceFile.read()
+        fragmentShaderSourceFile.close()
+        fragmentShader = glCreateShader(GL_FRAGMENT_SHADER)
+        glShaderSource(fragmentShader,fragmentShaderSource)
+        glCompileShader(fragmentShader)
+        fragmentCompileSuccess = glGetShaderiv(fragmentShader,GL_COMPILE_STATUS)
+        if not fragmentCompileSuccess:
+            infoLog = glGetShaderInfoLog(fragmentShader)
+            logging.critical(infoLog)
+            raise 'Unable to compile vertex shader'
+
+        shaderProgram = glCreateProgram()
+        glAttachShader(shaderProgram,vertexShader)
+        glAttachShader(shaderProgram,fragmentShader)
+        glLinkProgram(shaderProgram)
+        shaderProgramCompileSuccess = glGetProgramiv(shaderProgram,GL_LINK_STATUS)
+        if not shaderProgramCompileSuccess:
+            infoLog = ''
+            glGetProgramInfoLog(shaderProgram,infoLog)
+            logging.critical(infoLog)
+            raise 'Unable to link shader'
+        
+        self.shaderProgram = shaderProgram
+        glDeleteShader(vertexShader)
+        glDeleteShader(fragmentShader)
+        
+    def use(self):
+        glUseProgram(self.shaderProgram)
+
 
 def fogeInit(width,height,title): #foge初始化
     global window,shaderProgram
@@ -39,46 +86,12 @@ def fogeInit(width,height,title): #foge初始化
     glfwMakeContextCurrent(window)
     glfwSetFramebufferSizeCallback(window,change_framebuffer_size_callback)
 
-    vertexShaderSourceFile = open('./shader/Vertex.glsl','r')
-    vertexShaderSource = vertexShaderSourceFile.read()
-    vertexShaderSourceFile.close()
-    vertexShader = glCreateShader(GL_VERTEX_SHADER)
-    glShaderSource(vertexShader,vertexShaderSource)
-    glCompileShader(vertexShader)
-    vertexCompileSuccess = glGetShaderiv(vertexShader,GL_COMPILE_STATUS)
-    if not vertexCompileSuccess:
-        infoLog = glGetShaderInfoLog(vertexShader)
-        logging.critical(infoLog)
-        raise 'Unable to compile vertex shader'
-
-    fragmentShaderSourceFile = open('./shader/Fragment.glsl','r')
-    fragmentShaderSource = fragmentShaderSourceFile.read()
-    fragmentShaderSourceFile.close()
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER)
-    glShaderSource(fragmentShader,fragmentShaderSource)
-    glCompileShader(fragmentShader)
-    fragmentCompileSuccess = glGetShaderiv(fragmentShader,GL_COMPILE_STATUS)
-    if not fragmentCompileSuccess:
-        infoLog = glGetShaderInfoLog(fragmentShader)
-        logging.critical(infoLog)
-        raise 'Unable to compile vertex shader'
-
-    shaderProgram = glCreateProgram()
-    glAttachShader(shaderProgram,vertexShader)
-    glAttachShader(shaderProgram,fragmentShader)
-    glLinkProgram(shaderProgram)
-    shaderProgramCompileSuccess = glGetProgramiv(shaderProgram,GL_LINK_STATUS)
-    if not shaderProgramCompileSuccess:
-        infoLog = ''
-        glGetProgramInfoLog(shaderProgram,infoLog)
-        logging.critical(infoLog)
-        raise 'Unable to link shader'
-
 def fogeMainloop():
     global window,shaderProgram
+    glClearColor(0,0,0,1)
+    glPolygonMode(GL_FRONT_AND_BACK,GL_LINE)
     while not glfwWindowShouldClose(window):
         __fogeProcessKeyInput__(window)
-        glClearColor(0,0,0,1)
         glClear(GL_COLOR_BUFFER_BIT)
         glUseProgram(shaderProgram)
         __fogeDrawObj__()
@@ -93,6 +106,34 @@ def fogeClose():
 def fogeRegisterKey(key,status,function):
     global key_function_map
     key_function_map[key] = (status,function)
+
+class forgeModel:
+    def __init__(self,vertice_list=None,indices=None,usage=GL_STATIC_DRAW):
+        global draw_obj
+        self.type = 'model'
+        draw_obj.append(self)
+        if vertice_list and indices:
+            self.indices_num = len(indices)*3
+            self.VAO = glGenVertexArrays(1)
+            self.VBO = glGenBuffers(1)
+            self.EBO = glGenBuffers(1)
+            glBindVertexArray(self.VAO)
+            
+            all_indices = []
+            for i in indices:
+                all_indices = all_indices+i
+            all_list = []
+            for i in vertice_list:
+                all_list = all_list+i
+
+            glBindBuffer(GL_ARRAY_BUFFER,self.VBO)
+            glBufferData(GL_ARRAY_BUFFER,numpy.array(all_list,dtype='float32'),usage)
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,self.EBO)
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER,numpy.array(all_indices,dtype='uint32'),usage)
+            glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,None)
+            glEnableVertexAttribArray(0)
+            glBindBuffer(GL_ARRAY_BUFFER,0)
+            glBindVertexArray(0)
 
 class fogeTriangle:
     def __init__(self,vertice1,vertice2,vertice3,usage):
--- test.py
+++ test.py
@@ -3,7 +3,18 @@ from FOGE import *
 fogeInit(500,500,'qwe')
 
 fogeRegisterKey(GLFW_KEY_ESCAPE,GLFW_PRESS,fogeClose)
+'''
 triangle = fogeTriangle([-0.5,-0.5,0],[0,-0.5,0],[-0.25,0.5,0],GL_STATIC_DRAW)
 triangle = fogeTriangle([0.5,-0.5,0],[0,-0.5,0],[0.25,0.5,0],GL_STATIC_DRAW)
+'''
+vs = [[-0.5,0.5,0.5],[0.5,0.5,0.5],[0.5,-0.5,0.5],[-0.5,-0.5,0.5],
+      [-0.5,0.5,-0.5],[0.5,0.5,-0.5],[0.5,-0.5,-0.5],[-0.5,-0.5,-0.5]]
+is_ = [[0,1,3],[1,2,3],
+       [4,5,7],[5,6,7],
+       [0,4,7],[0,3,7],
+       [1,5,6],[1,2,6],
+       [0,1,5],[0,4,5],
+       [3,2,6],[3,7,6]]
+model = forgeModel(vs,is_)
 
 fogeMainloop()
